644d0084e9406bab887cbb931605c56643d6af0c
jtanumihardja <103391594+jtanumihardja@users.noreply.github.com>
2023-03-31 15:53:00 -0700

    update files

--- shinyApp/global.R
+++ shinyApp/global.R
@@ -6,3 +6,11 @@ library(ggiraph)
 source("www/functions/helper_functions.R")
 
 df_water_map <- read_csv('www/datasets/water_map_reduced.csv')
+
+water1 <- read_csv('www/datasets/water1.csv')
+water2 <- read_csv('www/datasets/water2.csv')
+
+sanitation1 <- read_csv('www/datasets/sanitation1.csv')
+sanitation2 <- read_csv('www/datasets/sanitation2.csv')
+
+hygiene1 <- read_csv('www/datasets/hygiene1.csv')
