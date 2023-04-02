2eedce12944b7bf72794f440c16dc8076a4cc4d1
JennyHan0108 <108483890+JennyHan0108@users.noreply.github.com>
2023-04-02 16:48:33 -0700

    Handle region/year observer for Sanitation and Hygiene tabs

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -4,29 +4,17 @@ server <- function(input, output){
                            year_end = NULL,
                            map_df = NULL)
   
-  values$df <- df_water
-  
-  # when the user changes tabs
-  observeEvent(input$navbar, {
+  observe({
     if (input$navbar == "Drinking Water") {
       values$df <- df_water
-    } else if (input$navbar == "Sanitation") {
-      values$df <- df_sanitation
-    } else if (input$navbar == "Hygiene") {
-      values$df <- df_hygiene
-    }
-  })
-  
-  
-  observe({
-    values$region <- input$dw_region
-    values$year_start <- input$dw_year[1]
-    values$year_end <- input$dw_year[2]
-    values$map_df <- df_water_map %>%
-      filter(Region == values$region & Year >= values$year_start & Year <= values$year_end) %>%
-      select(`COUNTRY, AREA OR TERRITORY`, long, lat, group, `At least basic`, `Limited (more than 30 mins)`, Unimproved, `Surface water`)
-    
-    if (input$navbar == "Drinking Water") {
+      values$region <- input$dw_region
+      values$year_start <- input$dw_year[1]
+      values$year_end <- input$dw_year[2]
+      values$map_df <- df_water_map %>%
+        filter(Region == values$region & Year >= values$year_start & Year <= values$year_end) %>%
+        select(`COUNTRY, AREA OR TERRITORY`, long, lat, group, `At least basic`, `Limited (more than 30 mins)`, Unimproved, `Surface water`)
+      
+      
       output$dw_map_plot <- renderGirafe(
         plot_map(values$map_df)
       )
@@ -35,10 +23,20 @@ server <- function(input, output){
         plot_line(values$df, values$region, values$year_start, values$year_end)
       })
     } else if (input$navbar == "Sanitation") {
+      values$df <- df_sanitation
+      values$region <- input$s_region
+      values$year_start <- input$s_year[1]
+      values$year_end <- input$s_year[2]
+      
       output$s_line_plot <- renderPlot({
         plot_line(values$df, values$region, values$year_start, values$year_end)
       })
     } else if (input$navbar == "Hygiene") {
+      values$df <- df_hygiene
+      values$region <- input$h_region
+      values$year_start <- input$h_year[1]
+      values$year_end <- input$h_year[2]
+      
       output$h_line_plot <- renderPlot({
         plot_line(values$df, values$region, values$year_start, values$year_end)
       })
