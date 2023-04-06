5efcfc79be248f25f4a2b0b459955e674c329f80
jtanumihardja <103391594+jtanumihardja@users.noreply.github.com>
2023-04-05 19:25:32 -0700

    add Icons on tabPanel, add "About"tab
    try out ggiraph for donut plot in data cleaning file. Will clean up more later.

--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -1,14 +1,18 @@
-library(shinythemes)
+#Drinking Water, Sanitation, and Hygiene Household UI
+#Team Shabu-shabu, IE6600, Spring 2023
 
 ui <- fluidPage(
+
   navbarPage(
     "Water, Sanitation & Hygiene (WASH)",
     id = "navbar",
     theme = shinytheme("flatly"), #chosen theme
     
+    ##Drinking Water Tab---------------------------------------------------
     tabPanel(
       "Drinking Water",
-      
+      icon = icon("glass-water-droplet"), #e4f4 https://fontawesome.com/icons/glass-water?f=classic&s=solid
+       #https://github.com/rstudio/fontawesome 
       sidebarPanel(
         awesomeRadio(
           inputId = "dw_geography",
@@ -76,8 +80,10 @@ ui <- fluidPage(
       )
     ),
     
+    ##Sanitation tab -------------------------------------------------------
     tabPanel(
       "Sanitation",
+      icon = icon("toilet"), #https://fontawesome.com/icons/toilet?f=classic&s=regular
       
       sidebarPanel(
         awesomeRadio(
@@ -146,8 +152,10 @@ ui <- fluidPage(
       )
     ),
     
+    ##Hygiene Tab ------------------------------------------------------
     tabPanel(
       "Hygiene",
+      icon = icon("soap"), #https://fontawesome.com/icons/soap?f=classic&s=regular
       
       sidebarPanel(
         awesomeRadio(
@@ -218,8 +226,12 @@ ui <- fluidPage(
       )
     ),
     
-    tabPanel("Modeling"),
+    ##Forecast Tab -------------------------------------------------
+    tabPanel("Forecast",
+             icon = icon("arrow-trend-up")), #https://fontawesome.com/icons/arrow-trend-up?f=classic&s=solid
     
-    tabPanel("About") 
+    ##About Tab -----------------------------------------------------
+    tabPanel("About",
+             icon = icon("github")) 
   )
 )
