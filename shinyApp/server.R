5b873b8dc8423f4465be9ce19fa392fc3ee6277a
jtanumihardja <103391594+jtanumihardja@users.noreply.github.com>
2023-04-06 16:08:21 -0700

    Update UI, Server, Helper. Add About.md
    UI update: add title and description for each tabsetPanel. update code for ggiraph donut plot.
    
    Server: add serviceType and plotColor according to each tabset.
    update code for ggiraph donut plot. delete direct link to Github from about.
    
    Helper function: update code for ggiraph donut plot.
    
    About.md: for About page

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -1,11 +1,11 @@
 server <- function(input, output){
   
-  #hyperlink for "About" tabPanel
-  observeEvent(input$navbar, {
-    if(input$navbar =="About"){
-      browseURL("https://github.com/TomTomoki/DrinkingWater-Sanitation-Hygiene-Visualization")
-    }
-  })
+  #hyperlink for "About" tabPanel - not used??
+  # observeEvent(input$navbar, {
+  #   if(input$navbar =="About"){
+  #     browseURL("https://github.com/TomTomoki/DrinkingWater-Sanitation-Hygiene-Visualization")
+  #   }
+  # })
   
   #creating plots
   values <- reactiveValues(geo = NULL,
@@ -17,14 +17,19 @@ server <- function(input, output){
                            map_df = NULL,
                            df = NULL,
                            df2 = NULL,
-                           geoTitle = NULL)
+                           geoTitle = NULL,
+                           plotColor = NULL,
+                           serviceType = NULL)
   
+  ##Drinking Water tab -------------------------------------------------------
   observe({
     if (input$navbar == "Drinking Water") {
+      values$serviceType <- "Drinking Water"
       values$geo <- input$dw_geography
       values$region <- input$dw_region
       values$year_start <- input$dw_year[1]
       values$year_end <- input$dw_year[2]
+      values$plotColor <- "Paired"
       
       values$map_df <- df_water_map %>%
         filter(Region == values$region & Year >= values$year_start & Year <= values$year_end) %>%
@@ -43,20 +48,20 @@ server <- function(input, output){
         values$df <- df_water %>% filter(SDGRegion == values$geoRegion)
         values$df2 <- df_water2 %>% filter(SDGRegion == values$geoRegion)
         values$geoTitle <- input$dw_geoRegion #for plot title
-        print(values$geoRegion) #troubleshoot
-        print(values$geoTitle)
+        # print(values$geoRegion) #for troubleshoot
+        # print(values$geoTitle)
       } else if(values$geo == "region_country"){
         values$country <- input$dw_region_country
         values$df <- df_water %>% filter(COUNTRY == values$country)
         values$df2 <- df_water2 %>% filter(COUNTRY == values$country)
         values$geoTitle <- input$dw_region_country #for plot title
-        print(values$country)  #troubleshoot
-        print(values$geoTitle)
+        # print(values$country)  #troubleshoot
+        # print(values$geoTitle) #for troubleshoot
       } else {
         values$df <- df_water
         values$df2 <- df_water2
         values$geoTitle <- values$geo
-        print(values$geoTitle)
+        # print(values$geoTitle) #for troubleshoot
       }
       #view(values$df) #for troubleshooting
       
@@ -91,26 +96,19 @@ server <- function(input, output){
       # )
       
       #plotting donut chart
-      output$dw_donut_plot <- renderPlot({
-        plot_donut(values$df, values$region, values$year_end, values$geoTitle)
-      })
-      
-      output$dw_downloadDonut <- downloadHandler(
-        filename = function() {
-          paste("plot_donut_dw_", values$geoTitle, Sys.Date(), ".png", sep="")
-        },
-        content = function(file) {
-          png(file=file)
-          plot(plot_donut(values$df, values$region, values$year_end, values$geoTitle))
-          dev.off()
-        }
+      output$dw_donut_plot <- renderGirafe(
+        plot_donut(values$df, values$region, values$year_end, 
+                   values$geoTitle, values$serviceType, values$plotColor)
       )
-    
+
+    ##Sanitation tab--------------------------------------------
     } else if (input$navbar == "Sanitation") {
+      values$serviceType <- "Sanitation"
       values$geo <- input$s_geography
       values$region <- input$s_region
       values$year_start <- input$s_year[1]
       values$year_end <- input$s_year[2]
+      values$plotColor <- "YlOrBr"
       
       values$map_df <- df_sanitation_map %>%
         filter(Region == values$region & Year >= values$year_start & Year <= values$year_end) %>%
@@ -129,21 +127,21 @@ server <- function(input, output){
         values$df <- df_sanitation %>% filter(SDGRegion == values$geoRegion)
         values$df2 <- df_sanitation2 %>% filter(SDGRegion == values$geoRegion)
         values$geoTitle <- input$s_geoRegion #for plot title
-        print(values$geoRegion) #troubleshoot
-        print(values$geoTitle) #troubleshoot
+        # print(values$geoRegion) #troubleshoot
+        # print(values$geoTitle) #troubleshoot
         
       } else if(values$geo == "region_country"){
         values$country <- input$s_region_country
         values$df <- df_sanitation %>% filter(COUNTRY == values$country)
         values$df2 <- df_sanitation2 %>% filter(COUNTRY == values$country)
         values$geoTitle <- input$s_region_country #for plot title
-        print(values$country)  #troubleshoot
-        print(values$geoTitle) #troubleshoot
+        # print(values$country)  #troubleshoot
+        # print(values$geoTitle) #troubleshoot
       } else {
         values$df <- df_sanitation
         values$df2 <- df_sanitation2
         values$geoTitle <- values$geo
-        print(values$geoTitle) #troubleshoot
+        #print(values$geoTitle) #troubleshoot
       }
       #view(values$df) #for troubleshooting
       
@@ -179,27 +177,20 @@ server <- function(input, output){
       # )
       
       #plotting donut chart
-      output$s_donut_plot <- renderPlot({
-        plot_donut(values$df, values$region, values$year_end, values$geoTitle)
-      })
-      
-      output$s_downloadDonut <- downloadHandler(
-        filename = function() {
-          paste("plot_donut_san_", values$geoTitle, Sys.Date(), ".png", sep="")
-        },
-        content = function(file) {
-          png(file=file)
-          plot(plot_donut(values$df, values$region, 
-                          values$year_end, values$geoTitle))
-          dev.off()
-        }
+      output$s_donut_plot <- renderGirafe(
+        plot_donut(values$df, values$region, values$year_end, 
+                   values$geoTitle, values$serviceType, values$plotColor)
       )
-      
+
+    
+    ##Hygiene tab -----------------------------------------------------  
     } else if (input$navbar == "Hygiene") {
+      values$serviceType <- "Hygiene"
       values$geo <- input$h_geography
       values$region <- input$h_region
       values$year_start <- input$h_year[1]
       values$year_end <- input$h_year[2]
+      values$plotColor <- "PuRd"
       
       values$map_df <- df_hygiene_map %>%
         filter(Region == values$region & Year >= values$year_start & Year <= values$year_end) %>%
@@ -218,21 +209,21 @@ server <- function(input, output){
         values$df <- df_hygiene %>% filter(SDGRegion == values$geoRegion)
         values$geoTitle <- input$h_geoRegion #for plot title
         
-        print(values$geoRegion) #troubleshoot
-        print(values$geoTitle) #troubleshoot
+        # print(values$geoRegion) #troubleshoot
+        # print(values$geoTitle) #troubleshoot
         
       } else if(values$geo == "region_country"){
         values$country <- input$h_region_country
         values$df <- df_hygiene %>% filter(COUNTRY == values$country)
         values$geoTitle <- input$h_region_country #for plot title
         
-        print(values$country)  #troubleshoot
-        print(values$geoTitle) #troubleshoot
+        # print(values$country)  #troubleshoot
+        # print(values$geoTitle) #troubleshoot
       } else {
         values$df <- df_hygiene
         values$geoTitle <- values$geo
         
-        print(values$geoTitle) #troubleshoot
+        #print(values$geoTitle) #troubleshoot
       }
       #view(values$df) #for troubleshooting
       
@@ -267,20 +258,9 @@ server <- function(input, output){
       # )
       
       #plotting donut chart
-      output$h_donut_plot <- renderPlot({
-        plot_donut(values$df, values$region, values$year_end, values$geoTitle)
-      })
-      
-      output$h_downloadDonut <- downloadHandler(
-        filename = function() {
-          paste("plot_donut_hyg_", values$geoTitle, Sys.Date(), ".png", sep="")
-        },
-        content = function(file) {
-          png(file=file)
-          plot(plot_donut(values$df, values$region, 
-                          values$year_end, values$geoTitle))
-          dev.off()
-        }
+      output$h_donut_plot <- renderGirafe(
+        plot_donut(values$df, values$region, values$year_end, 
+                   values$geoTitle, values$serviceType, values$plotColor)
       )
       
     }
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -13,7 +13,10 @@ ui <- fluidPage(
       "Drinking Water",
       icon = icon("glass-water-droplet"), #e4f4 https://fontawesome.com/icons/glass-water?f=classic&s=solid
        #https://github.com/rstudio/fontawesome 
+      
       sidebarPanel(
+        helpText("Create WHO/UNICEF JPM household plots based on selected geography, classification area, and year range."),
+        
         awesomeRadio(
           inputId = "dw_geography",
           label = "Geography:",
@@ -67,16 +70,28 @@ ui <- fluidPage(
       ),
       
       mainPanel(
-        girafeOutput(outputId = "dw_map_plot"),
-        plotOutput(outputId = "dw_line_plot"),
-        downloadButton("dw_downloadLine", "Download Line Plot", 
+        fluidPage(
+          h2("Drinking Water"),
+          h4("Drinking water service can be classified into 4 levels (from best to worst): "),
+          p("- At least basic = 'Drinking water from an improved source, provided collection time is not more than 30 minutes for a round trip, including queuing'"),
+          p("- Limited(>30min) = 'Drinking water from an improved source for which collection time exceeds 30 minutes for a round trip, including queuing'"),
+          p("- Unimproved = 'Drinking water from an unprotected dug well or unprotected spring'"),
+          p("- Surface Water = 'Drinking water directly from a river, dam, lake, pond, stream, canal or irrigation canal'"),
+          br(),
+          br(),
+          girafeOutput(outputId = "dw_map_plot"),
+          br(),
+          plotOutput(outputId = "dw_line_plot"),
+          downloadButton("dw_downloadLine", "Download Line Plot", 
                        icon = shiny::icon("download")),
-        plotOutput(outputId = "dw_donut_plot"),
-        downloadButton("dw_downloadDonut", "Download Donut Plot", 
-                       icon = shiny::icon("download")),
-        plotOutput(outputId = "dw_bar_plot"),
-        downloadButton("dw_downloadBar", "Download Bar Plot", 
+          br(),
+          girafeOutput(outputId = "dw_donut_plot"),
+          br(),
+          plotOutput(outputId = "dw_bar_plot"),
+          downloadButton("dw_downloadBar", "Download Bar Plot", 
                        icon = shiny::icon("download"))
+        )
+        
       )
     ),
     
@@ -86,6 +101,8 @@ ui <- fluidPage(
       icon = icon("toilet"), #https://fontawesome.com/icons/toilet?f=classic&s=regular
       
       sidebarPanel(
+        helpText("Create WHO/UNICEF JPM household plots based on selected geography, classification area, and year range."),
+        
         awesomeRadio(
           inputId = "s_geography",
           label = "Geography:",
@@ -139,16 +156,31 @@ ui <- fluidPage(
       ),
       
       mainPanel(
-        girafeOutput(outputId = "s_map_plot"),
-        plotOutput(outputId = "s_line_plot"),
-        downloadButton("s_downloadLine", "Download Line Plot", 
-                       icon = shiny::icon("download")),
-        plotOutput(outputId = "s_donut_plot"),
-        downloadButton("s_downloadDonut", "Download Donut Plot", 
-                       icon = shiny::icon("download")),
-        plotOutput(outputId = "s_bar_plot"),
-        downloadButton("s_downloadBar", "Download Bar Plot", 
-                       icon = shiny::icon("download"))
+        fluidPage(
+          h2("Sanitation"),
+          h4("Sanitation service can be classified into 4 levels (from best to worst): "),
+          p("- At least basic = 'Use of improved facilities that are not shared with other households'"),
+          p("- Limited = 'Use of improved facilities shared between two or more households'"),
+          p("- Unimproved = 'Use of pit latrines without a slab or platform, hanging latrines or bucket latrines'"),
+          p("- Open Defecation = 'Disposal of human faeces in fields, forests, bushes, open bodies of water, beaches or other open spaces, or with solid waste'"),
+          br(),
+          br(),
+          girafeOutput(outputId = "s_map_plot"),
+          br(),
+          br(),
+          plotOutput(outputId = "s_line_plot"),
+          downloadButton("s_downloadLine", "Download Line Plot", 
+                         icon = shiny::icon("download")),
+          br(),
+          br(),
+          girafeOutput(outputId = "s_donut_plot"),
+          br(),
+          br(),
+          plotOutput(outputId = "s_bar_plot"),
+          downloadButton("s_downloadBar", "Download Bar Plot", 
+                         icon = shiny::icon("download"))
+        )
+        
       )
     ),
     
@@ -158,6 +190,8 @@ ui <- fluidPage(
       icon = icon("soap"), #https://fontawesome.com/icons/soap?f=classic&s=regular
       
       sidebarPanel(
+        helpText("Create WHO/UNICEF JPM household plots based on selected geography, classification area, and year range."),
+        
         awesomeRadio(
           inputId = "h_geography",
           label = "Geography:",
@@ -211,18 +245,26 @@ ui <- fluidPage(
       ),
       
       mainPanel(
-        girafeOutput(outputId = "h_map_plot"),
-        plotOutput(outputId = "h_line_plot"),
-        downloadButton("h_downloadLine", "Download Line Plot", 
-                       icon = shiny::icon("download")),
-        
-        plotOutput(outputId = "h_donut_plot"),
-        downloadButton("h_downloadDonut", "Download Donut Plot", 
-                      icon = shiny::icon("download")),
-        
-        plotOutput(outputId = "h_bar_plot"),
-        downloadButton("h_downloadBar", "Download Bar Plot", 
-                       icon = shiny::icon("download"))
+        fluidPage(
+          h2("Hygiene"),
+          h4("Hygiene service can be classified into 3 levels (from best to worst): "),
+          p("- At least basic = 'Availability of a handwashing facility on premises with soap and water'"),
+          p("- Limited = 'Availability of handwashing facility on premises without soap and water'"),
+          p("- No Facility = 'No handwashing facility on premises'"),
+          br(),
+          br(),
+          girafeOutput(outputId = "h_map_plot"),
+          br(),
+          plotOutput(outputId = "h_line_plot"),
+          downloadButton("h_downloadLine", "Download Line Plot", 
+                         icon = shiny::icon("download")),
+          br(),
+          girafeOutput(outputId = "h_donut_plot"),
+          br(),
+          plotOutput(outputId = "h_bar_plot"),
+          downloadButton("h_downloadBar", "Download Bar Plot", 
+                         icon = shiny::icon("download"))
+        )
       )
     ),
     
@@ -232,6 +274,8 @@ ui <- fluidPage(
     
     ##About Tab -----------------------------------------------------
     tabPanel("About",
-             icon = icon("github")) 
+             icon = icon("github"),
+             includeMarkdown("www/datasets/About.Rmd") #https://shiny.rstudio.com/gallery/biodiversity-national-parks.html 
+               ) 
   )
 )
